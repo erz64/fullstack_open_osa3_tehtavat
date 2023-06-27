7ff9846e8a96f949c50fa849ee7b5c30231ed4fe
erz64 <eeroranta111@gmail.com>
2023-06-27 17:18:54 +0300

    phonebook should now look up the data from the mongodb database

--- models/person.js
+++ models/person.js
@@ -18,7 +18,7 @@ const personSchema = new mongoose.Schema({
   number: Number,
 })
 
-noteSchema.set('toJSON', {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
