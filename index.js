734db814618db2930d75d085d701c7a7b18d1708
erz64 <eeroranta111@gmail.com>
2023-06-28 16:13:25 +0300

    updating persons number works with the database now

--- index.js
+++ index.js
@@ -102,6 +102,22 @@ app.post('/api/persons', (req, res) => {
 
 })
 
+app.put('/api/persons/:id'), (req, res) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true})
+    .then(updatedPerson => {
+        res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+}
+
+
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
 
